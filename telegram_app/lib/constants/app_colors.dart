cce907c0e5721f5cc9179b600c83f736f7b11908
terlandurden <senahuseynova1@gmail.com>
2025-04-14 12:42:27 +0400

    latest version

---
+++ telegram_app/lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+///this is all colors of app basqa seyden istifade eleme
+class AppColors {
+  AppColors._();
+
+  //example
+  static const Color primaryColor = Colors.purple;
+}
