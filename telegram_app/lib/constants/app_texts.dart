2eea7210054755ec46b77e7d271b4fe6bb9754b2
Gulara Gulmammadova <gularagulmammadova@gmail.com>
2025-04-19 14:25:49 +0400

    TelegramNotifications Screen Done

---
+++ telegram_app/lib/constants/app_texts.dart
@@ -0,0 +1,19 @@
+class AppTexts {
+  AppTexts._();
+  
+  ///Telegram Notifications Texts @GULARA
+  static const String allAccounts = "All Accounts";
+  static const String showNotificationsFrom = "Show Notifications From";
+  static const String turnOffToReceiveNotifications = "Turn this off if you want to receive notifications only from your active account.";
+  static const String messageNotifications = "Message Notifications";
+  static const String showNotifications = "Show Notifications";
+  static const String messagePreview = "Message Preview";
+  static const String sound = "Sound";
+  static const String exceptions = "Exceptions";
+  static const String chats = "66 Chats";
+  static const String setCustomNotifications = "Set custom notifications for specific users.";
+  static const String groupShowNotifications = "Show Notifications for Groups";
+  static const String soundNone = "None";
+  static const String exceptionsText = "Exceptions";
+  static const String addText = "Add";
+}
---
+++ telegram_app/lib/utils/widgets/custom_switchtile_item.dart
@@ -0,0 +1,9 @@
+class CustomSwitchtileItem {
+  final String title;
+  bool value;
+
+  CustomSwitchtileItem({
+    required this.title,
+    required this.value,
+  });
+}
